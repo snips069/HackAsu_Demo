d095eaeb7f323dfaba78d3b916aa45591b5216d6
snips069 <snips049@gmail.com>
2025-11-09 05:00:27 -0700

    SCRUM-15: Fix refactor issue

--- src/config/loader-refactored.ts
+++ src/config/loader-refactored.ts
@@ -1,6 +1,8 @@
 // Extract loader into modular pipeline
+// Restore missing cache warmup
 export class ConfigLoaderPipeline {
   private steps: Array<(config: any) => any> = [];
+  private cache: Map<string, any> = new Map();
 
   addStep(step: (config: any) => any) {
     this.steps.push(step);
@@ -8,10 +10,15 @@ export class ConfigLoaderPipeline {
   }
 
   async load() {
+    const cacheKey = 'config';
+    if (this.cache.has(cacheKey)) {
+      return this.cache.get(cacheKey);
+    }
     let config = {};
     for (const step of this.steps) {
       config = await step(config);
     }
+    this.cache.set(cacheKey, config);
     return config;
   }
 }
